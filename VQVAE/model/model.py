a93d378685c8541e59db5da2b99df46d17b21e94
root <benzelongji@gmail.com>
2025-09-03 14:02:59 +0900

    Modified to contain a VQVAE

--- VQVAE/model/model.py
+++ VQVAE/model/model.py
@@ -23,24 +23,37 @@ class MLP(nn.Module):
         self.vq_emb_dim = kwargs['vqvae_embedding_dim']
         self.vq_beta = kwargs['vqvae_beta']
         # self.vqvae_path = kwargs['vqvae_path']
+        
+        # Convのパラメータ
+        self.conv_dim = kwargs['conv_dim']
+        self.conv_kernel_size = kwargs['conv_kernel_size']
+        self.conv_pad = kwargs['conv_padding']
+        self.conv_str = kwargs['conv_stride']
+
+        self.pool_kernel_size = kwargs['pool_kernel_size']
+        self.pool_str = kwargs['pool_stride']
 
         # VQVAEの用意
         self.vqvae = VQVAE(self.vq_h_dim,
                            self.vq_res_h_dim,
                            self.vq_n_res,
-                           self.vq_n_emb
+                           self.vq_n_emb,
                            self.vq_emb_dim,
                            self.vq_beta,)
         
+        # pathベタ打ちなの嫌、どうにかならないか
         checkpoint = torch.load('VQVAE_local.pth', weights_only=True)
         self.vqvae.load_state_dict(checkpoint['param'])
-        
-        # 埋め込み層の用意
-        self.embedding = nn.Embedding(self.vq_n_emb, self.emb_dim)
+
+        self.codebook = self.vqvae.vector_quantization.embedding.weight
+
+        self.conv = nn.Conv2d(self.vq_emb_dim, self.conv_dim, kernel_size=self.conv_kernel_size, padding=self.conv_pad, stride=self.conv_str)
+        self.pool = nn.MaxPool2d(kernel_size=self.pool_kernel_size, stride=self.pool_str)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
 
         # その他MLPの用意
         self.layers = nn.Sequential()
-        self.layers.append(nn.Linear(self.input_size*self.emb_dim, self.top_hidden_size))
+        self.layers.append(nn.Linear(self.conv_dim, self.top_hidden_size))
         self.layers.append(nn.ReLU())
         for i in range(int((self.number_of_layers-2)/2)):
             self.layers.append(nn.Linear(self.top_hidden_size, self.top_hidden_size))
@@ -56,12 +69,24 @@ class MLP(nn.Module):
     def forward(self, x):
         # VQVAEを通し潜在表現を取得
         _, _, x = self.vqvae(x)
-        x = x.view(-1, self.input_size)
+    
+        min_encodings = torch.zeros(x.shape[0], self.vq_n_emb, device=x.device)
+        min_encodings.scatter_(1, x, 1)
+        x = torch.matmul(min_encodings, self.codebook)
+        # print(x.shape)
 
-        # 埋め込みを行う
-        x = self.embedding(x)
+        x = x.view(-1, 64, 64, 64)
+        # x = x.view(-1, self.input_size)
+        # print(x.shape)
+
+        x = x.permute(0, 3, 1, 2)
+        x = self.conv(x)
+        x = self.pool(x)
+
+        x = self.gap(x)
 
         # 次元のベクトルに変換
+        x = x.permute(0, 2, 3, 1)
         x = x.view(x.size(0), -1)
         # print(x.shape)
 
