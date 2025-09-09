d6502347b0b384ab67e51ed57247d4ff4924b45e
root <benzelongji@gmail.com>
2025-09-09 16:19:56 +0900

    get some losses

--- VQVAE/src/trainer.py
+++ VQVAE/src/trainer.py
@@ -18,36 +18,51 @@ class Trainer:
     def train(self):
         for i in tqdm(range(self.epoch, self.max_epochs + 1)):
             train_loss = 0
+            train_emb_loss = 0
+            train_recon_loss = 0
             test_loss = 0
+            test_emb_loss = 0
+            test_recon_loss = 0
             self.epoch = i
 
             # 訓練
             self.model.train()
+            self.optimizer.train()
             for img, _ in self.train_loader:
                 img = img.to(self.device, dtype=torch.float)
                 self.optimizer.zero_grad()
-                embedding_loss, x_hat, _ = self.model(img)
+                embedding_loss, x_hat, *_ = self.model(img)
                 recon_loss = nn.MSELoss()(x_hat, img)
                 loss = recon_loss + embedding_loss
                 train_loss += loss.item()
+                train_emb_loss += embedding_loss.item()
+                train_recon_loss += recon_loss.item()
                 loss.backward()
                 self.optimizer.step()
 
             # 評価
             self.model.eval()
+            self.optimizer.eval()
             with torch.no_grad():
                 for img_t, _ in self.test_loader:
                     img = img_t.to(self.device, dtype=torch.float)
-                    embedding_loss, x_hat, _ = self.model(img)
+                    embedding_loss, x_hat, *_ = self.model(img)
                     recon_loss = nn.MSELoss()(x_hat, img)
                     loss = recon_loss + embedding_loss
                     test_loss += loss.item()
+                    test_emb_loss += embedding_loss.item()
+                    test_recon_loss += recon_loss.item()
 
             # 損失の記録と表示
             train_loss /= len(self.train_loader.dataset)
+            train_emb_loss /= len(self.train_loader.dataset)
+            train_recon_loss /= len(self.train_loader.dataset)
             test_loss /= len(self.test_loader.dataset)
+            test_emb_loss /= len(self.test_loader.dataset)
+            test_recon_loss /= len(self.test_loader.dataset)
             print(f'epoch {i} train_loss: {train_loss:.5f} test_loss: {test_loss:.5f}')
-            self.run.log({"train_loss": train_loss, "test_loss": test_loss})
+            self.run.log({"train_loss": train_loss, "train_emb_loss": train_emb_loss, "train_recon_loss": train_recon_loss,
+                          "test_emb_loss": test_emb_loss, "test_recon_loss": test_recon_loss, "test_loss": test_loss})
             self.train_loss_log.append(train_loss)
             self.test_loss_log.append(test_loss)
 
