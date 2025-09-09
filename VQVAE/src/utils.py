a85c0d369fe4c611fcd1f93f797e75fc44cd7023
root <benzelongji@gmail.com>
2025-09-09 16:20:27 +0900

    made init_wandb

--- VQVAE/src/utils.py
+++ VQVAE/src/utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import wandb
 
 def plot_loss(train_loss_log, test_loss_log):
     plt.suptitle('Loss')
@@ -7,3 +8,19 @@ def plot_loss(train_loss_log, test_loss_log):
     plt.grid(axis='y')
     plt.legend()
     plt.savefig("/workspace/inhouse-vqvae/VQVAE/results/test.png")
+
+def init_wandb(cfg):
+    if "wandb" not in cfg:
+        print("A")
+        return wandb.init(mode="disabled")
+
+    wandb_kwargs = {
+        "entity": cfg.wandb.entity,
+        "project": cfg.wandb.project,
+        # "resume": "auto",
+    }
+
+    if hasattr(cfg.wandb, 'name'):
+        wandb_kwargs["name"] = cfg.wandb.name
+
+    return wandb.init(config=dict(cfg.model), **wandb_kwargs)
