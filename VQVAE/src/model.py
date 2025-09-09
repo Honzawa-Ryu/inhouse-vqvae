f809ad618d1902eab0dce2955003ab508bbcbf39
root <benzelongji@gmail.com>
2025-09-09 16:19:00 +0900

    made vqvae2 and refactor VQVAE init to use kwargs

--- VQVAE/src/model.py
+++ VQVAE/src/model.py
@@ -83,19 +83,46 @@ class Decoder(nn.Module):
             ResidualStack(h_dim, h_dim, res_h_dim, n_res_layers),
             nn.ConvTranspose2d(h_dim, h_dim // 2, kernel_size=kernel, stride=stride, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(h_dim // 2, 1, kernel_size=kernel, stride=stride, padding=1)
+            nn.ConvTranspose2d(h_dim // 2, 3, kernel_size=kernel, stride=stride, padding=1)
+        )
+
+    def forward(self, x):
+        return self.inverse_conv_stack(x)
+    
+
+class FakeDecoder(nn.Module):
+    # 二段階目以上のVQVAEモジュールのデコーダ、画像サイズに戻すのではなく一段階上の潜在表現に戻す
+    # なんか適当にやったので三層以上にしたとき齟齬が出るかも、要確認
+    def __init__(self, in_dim, h_dim, n_res_layers, res_h_dim):
+        super().__init__()
+        kernel = 4
+        stride = 2
+        self.inverse_conv_stack = nn.Sequential(
+            nn.ConvTranspose2d(in_dim, h_dim, kernel_size=kernel - 1, stride=stride - 1, padding=1),
+            ResidualStack(h_dim, h_dim, res_h_dim, n_res_layers),
+            nn.ConvTranspose2d(h_dim, h_dim, kernel_size=kernel, stride=stride, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(h_dim, h_dim // 2, kernel_size=kernel, stride=stride, padding=1)
         )
 
     def forward(self, x):
         return self.inverse_conv_stack(x)
 
+
 class VQVAE(nn.Module):
-    def __init__(self, h_dim, res_h_dim, n_res_layers, n_embeddings, embedding_dim, beta):
+    def __init__(self, **kwargs):
         super(VQVAE, self).__init__()
-        self.encoder = Encoder(1, h_dim, n_res_layers, res_h_dim)
-        self.pre_quantization_conv = nn.Conv2d(h_dim, embedding_dim, kernel_size=1, stride=1)
-        self.vector_quantization = VectorQuantizer(n_embeddings, embedding_dim, beta)
-        self.decoder = Decoder(embedding_dim, h_dim, n_res_layers, res_h_dim)
+        self.h_dim = kwargs['vqvae_h_dim']
+        self.res_h_dim = kwargs['vqvae_res_h_dim']
+        self.n_res = kwargs['vqvae_n_res_layers']
+        self.n_emb = kwargs['vqvae_n_embeddings']
+        self.emb_dim = kwargs['vqvae_embedding_dim']
+        self.beta = kwargs['vqvae_beta']
+
+        self.encoder = Encoder(3, self.h_dim, self.n_res, self.res_h_dim)
+        self.pre_quantization_conv = nn.Conv2d(self.h_dim, self.emb_dim, kernel_size=1, stride=1)
+        self.vector_quantization = VectorQuantizer(self.n_emb, self.emb_dim, self.beta)
+        self.decoder = Decoder(self.emb_dim, self.h_dim, self.n_res, self.res_h_dim)
 
     def forward(self, x):
         z_e = self.encoder(x)
@@ -104,3 +131,46 @@ class VQVAE(nn.Module):
         x_hat = self.decoder(z_q)
         return embedding_loss, x_hat, idx
 
+class VQVAE2(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.h_dim = kwargs['vqvae_h_dim']
+        self.res_h_dim = kwargs['vqvae_res_h_dim']
+        self.n_res = kwargs['vqvae_n_res_layers']
+        self.n_emb = kwargs['vqvae_n_embeddings']
+        self.emb_dim = kwargs['vqvae_embedding_dim']
+        self.beta = kwargs['vqvae_beta']
+
+        self.encoder_top = Encoder(3, self.h_dim, self.n_res, self.res_h_dim)
+        self.pre_quantization_conv_top = nn.Conv2d(self.h_dim, self.emb_dim, kernel_size=1, stride=1)
+        self.vector_quantization_top = VectorQuantizer(self.n_emb, self.emb_dim, self.beta)
+        self.decoder_top = Decoder(self.emb_dim, self.h_dim, self.n_res, self.res_h_dim)
+        self.encoder_bottom = Encoder(self.emb_dim, self.h_dim, self.n_res, self.res_h_dim)
+        self.pre_quantization_conv_bottom = nn.Conv2d(self.h_dim, self.emb_dim, kernel_size=1, stride=1)
+        self.vector_quantization_bottom = VectorQuantizer(self.n_emb, self.emb_dim, self.beta)
+        self.decoder_bottom = FakeDecoder(self.emb_dim, self.h_dim, self.n_res, self.res_h_dim)
+
+        self.summary = nn.Conv2d(self.emb_dim * 2, self.emb_dim, 3, 1, padding="same")
+
+
+    def forward(self, x):
+        z_e = self.encoder_top(x)
+
+        z_e_top = self.pre_quantization_conv_top(z_e)
+        embedding_loss_top, z_q_top, _, idx_top = self.vector_quantization_top(z_e_top)
+
+        z_e_e = self.encoder_bottom(z_e_top)
+        z_e_bottom = self.pre_quantization_conv_bottom(z_e_e)
+        embedding_loss_bottom, z_q_bottom, _, idx_bottom = self.vector_quantization_bottom(z_e_bottom)
+
+        x_hat_bottom = self.decoder_bottom(z_q_bottom)
+
+        pre_x_hat = torch.cat([z_q_top, x_hat_bottom], dim=1)
+        pre_x_hat = self.summary(pre_x_hat)
+
+        x_hat = self.decoder_top(pre_x_hat)
+        return embedding_loss_bottom + embedding_loss_top, x_hat, [idx_top, idx_bottom], pre_x_hat
+
+
+# だいぶ雑な実装ですが動くには動きそう、データ確保したら動かす
+# 今後やることとしてはまずエンコーダーの次元を調節可能にすること、三層以上に拡張可能にすること
